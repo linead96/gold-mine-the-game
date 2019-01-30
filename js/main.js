bf23285d256ec6d8942ac7c660d05fb121669620
Linead96 <danielgarboza@yahoo.com>
2019-01-30 23:37:46 +0800

    Added score and life left labels. Updated the limit on number of gold picked.  Change the default number of boxes to 8 x 8.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 let tds = document.querySelectorAll("td");
 let resetBtn = document.querySelector("#reset");
 let table = document.querySelector("table");
-let boxDimension = document.querySelector("select")
+let boxDimension = document.querySelector("select");
+let scoreOutput = document.querySelector("#score");
+let lifeLeftOutput = document.querySelector("#lifeleft");
 
 let randomBombBoxIndex = [];
 let randomGoldBoxIndex = [];
 let bombPicked = 0;
 let goldPicked = 0;
-let numOfBoxes = 3;
+let numOfBoxes = 8;
+let numOfBombAndGold = 0;
 
 const generateBoxes = () => {
     table.innerHTML = "";
@@ -25,12 +28,13 @@ const generateRandomBombBoxIndex = () => {
         do{
             randomNum = Math.round(Math.random()*(numOfBoxes*numOfBoxes)-1,2);
         }
-        while(randomBombBoxIndex.includes(randomNum));
+        while(randomBombBoxIndex.includes(randomNum) || randomNum < 0);
         randomBombBoxIndex.push(randomNum);
     }
     randomBombBoxIndex.forEach((item,index) => {
-        // console.log(index,item);
+     console.log("bomb",index,item);
     });
+    
 }
 
 const generateRandomGoldBoxIndex = () => {
@@ -39,9 +43,12 @@ const generateRandomGoldBoxIndex = () => {
         do{
             randomNum = Math.round(Math.random()*(numOfBoxes*numOfBoxes)-1,2);
         }
-        while(randomBombBoxIndex.includes(randomNum) || randomGoldBoxIndex.includes(randomNum));
+        while(randomBombBoxIndex.includes(randomNum) || randomGoldBoxIndex.includes(randomNum) || randomNum < 0);
         randomGoldBoxIndex.push(randomNum);
     }
+    randomGoldBoxIndex.forEach((item,index) => {
+        console.log("gold",index,item);
+    });
 }
 
 const addEventToTds = () => {
@@ -50,13 +57,14 @@ const addEventToTds = () => {
         if(randomBombBoxIndex.includes(index)){
             element.classList.add("bomb");
             bombPicked++; 
+            lifeLeftOutput.value = 3-bombPicked;
         }else if(randomGoldBoxIndex.includes(index)){
             element.classList.add("gold");
             goldPicked++;
+            scoreOutput.value = goldPicked;
         }else{
             element.classList.add("soil");
         }
-
         if(bombPicked === 3){
             tds.forEach((element, index) => {
                 element.className = "";
@@ -66,15 +74,6 @@ const addEventToTds = () => {
                 alert("Boom! Game Over!");
             },1);
         }
-        if(goldPicked === 3){
-            tds.forEach((element, index) => {
-                element.className = "";
-                element.classList.add("gold");
-            });
-            setTimeout(() => {
-                alert("Wow! You won!");
-            },1);
-        } 
     })
     });
 }
@@ -99,6 +98,8 @@ const resetValues = () => {
     randomGoldBoxIndex = [];
     bombPicked = 0;
     goldPicked = 0;
+    scoreOutput.value = goldPicked;
+    lifeLeftOutput.value = 3-bombPicked;
 }
 
 const resetAll = () => {
